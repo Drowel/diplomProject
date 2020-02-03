bbcf47a9feb82710c8fa06c45cf50558739f8143
Artsem Novikov <novikov.drow@gmail.com>
2020-02-03 11:19:09 +0300

    first commit

---
+++ src/main/java/com/example/hemacounter1/CounterActivity.kt
@@ -0,0 +1,12 @@
+package com.example.hemacounter1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CounterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_counter)
+    }
+}
